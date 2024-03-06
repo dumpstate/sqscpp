d939b4bcb21ea924ee9a03f5eab0daee6577126b
Albert Sadowski <asadowski@mailbox.org>
2024-03-06 07:05:36 +0000

    pull in googletest and unit test cli_args

--- Makefile
+++ Makefile
@@ -7,5 +7,8 @@ clean:
 	rm -rf target
 
 build:
-	cmake -B ./target -S . "-DCMAKE_TOOLCHAIN_FILE=${VCPKG_HOME}/scripts/buildsystems/vcpkg.cmake"
+	cmake -B ./target -S . "-DCMAKE_TOOLCHAIN_FILE=${VCPKG_HOME}/scripts/buildsystems/vcpkg.cmake" -DBoost_NO_WARN_NEW_VERSIONS=1
 	cd target && make && cd ..
+
+test: build
+	cd target && ctest && cd ..
---
+++ src/cli_args_test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include "./cli_args.cpp"
+
+using namespace sqscpp;
+
+std::vector<char*> as_argv(std::vector<std::string> args) {
+    std::vector<char*> argv;
+    for (const auto& arg : args)
+        argv.push_back((char*)arg.data());
+    argv.push_back(nullptr);
+    return argv;
+}
+
+TEST(cli_args_test, parse_cli_args_parse_help) {
+    auto argv = as_argv({"sqscpp", "--help"});
+    auto res = parse_cli_args(argv.size() - 1, argv.data());
+
+    EXPECT_EQ(res.first, false);
+}
+
+TEST(cli_args_test, parse_cli_args_parse_port) {
+    auto argv = as_argv({"sqscpp", "--port", "9999"});
+    auto res = parse_cli_args(argv.size() - 1, argv.data());
+
+    EXPECT_EQ(res.first, true);
+    EXPECT_EQ(res.second.port, 9999);
+    EXPECT_EQ(res.second.host, "0.0.0.0");
+}
+
+TEST(cli_args_test, parse_cli_args_parse_host) {
+    auto argv = as_argv({"sqscpp", "--host", "localhost"});
+    auto res = parse_cli_args(argv.size() - 1, argv.data());
+
+    EXPECT_EQ(res.first, true);
+    EXPECT_EQ(res.second.host, "localhost");
+    EXPECT_EQ(res.second.port, 8080);
+}
+
+TEST(cli_args_test, parse_cli_args_parse_host_and_port) {
+    auto argv = as_argv({"sqscpp", "--host", "localhost", "--port", "9999"});
+    auto res = parse_cli_args(argv.size() - 1, argv.data());
+
+    EXPECT_EQ(res.first, true);
+    EXPECT_EQ(res.second.host, "localhost");
+    EXPECT_EQ(res.second.port, 9999);
+}

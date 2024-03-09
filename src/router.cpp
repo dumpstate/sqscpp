ad6fc73b5d04174ab88166d62ddb675151bb00e1
Albert Sadowski <asadowski@mailbox.org>
2024-03-09 06:50:11 +0000

    run clang-format

--- src/router.cpp
+++ src/router.cpp
@@ -1,29 +1,32 @@
 #include "router.hpp"
 
 namespace sqscpp {
-    restinio::request_handling_status_t handler(restinio::request_handle_t req) {
-        auto headers = req->header();
-        auto protocol = parse_protocol(&headers);
+restinio::request_handling_status_t handler(restinio::request_handle_t req) {
+  auto headers = req->header();
+  auto protocol = parse_protocol(&headers);
 
-        req->create_response()
-            .set_body("AAA(protocol: " + to_str(protocol) + ")")
-            .done();
-        return restinio::request_accepted();
-    }
-
-    AWSProtocol parse_protocol(restinio::http_request_header_t* headers) {
-        auto content_type = headers->opt_value_of(restinio::http_field::content_type);
-        if (content_type.has_value() && content_type.value() == AWS_JSON_PROTOCOL_1_0) {
-            return AWSJsonProtocol1_0;
-        }
-
-        return AWSQueryProtocol;
-    }
-
-    std::string to_str(AWSProtocol protocol) {
-        switch (protocol) {
-            case AWSQueryProtocol: return "AWSQueryProtocol";
-            case AWSJsonProtocol1_0: return "AWSJsonProtocol1.0";
-        }
-    }
+  req->create_response()
+      .set_body("AAA(protocol: " + to_str(protocol) + ")")
+      .done();
+  return restinio::request_accepted();
 }
+
+AWSProtocol parse_protocol(restinio::http_request_header_t* headers) {
+  auto content_type = headers->opt_value_of(restinio::http_field::content_type);
+  if (content_type.has_value() &&
+      content_type.value() == AWS_JSON_PROTOCOL_1_0) {
+    return AWSJsonProtocol1_0;
+  }
+
+  return AWSQueryProtocol;
+}
+
+std::string to_str(AWSProtocol protocol) {
+  switch (protocol) {
+    case AWSQueryProtocol:
+      return "AWSQueryProtocol";
+    case AWSJsonProtocol1_0:
+      return "AWSJsonProtocol1.0";
+  }
+}
+}  // namespace sqscpp
--- src/router.hpp
+++ src/router.hpp
@@ -6,10 +6,7 @@
 namespace sqscpp {
 const std::string AWS_JSON_PROTOCOL_1_0 = "application/x-amz-json-1.0";
 
-enum AWSProtocol {
-    AWSQueryProtocol,
-    AWSJsonProtocol1_0
-};
+enum AWSProtocol { AWSQueryProtocol, AWSJsonProtocol1_0 };
 
 restinio::request_handling_status_t handler(restinio::request_handle_t req);
 AWSProtocol parse_protocol(restinio::http_request_header_t* headers);

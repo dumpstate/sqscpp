b284f3f29c649f808d2735c00b66b1b8c52955f1
Albert Sadowski <asadowski@mailbox.org>
2024-03-09 07:33:21 +0000

    parse sqs action

--- src/router.cpp
+++ src/router.cpp
@@ -3,22 +3,63 @@
 namespace sqscpp {
 restinio::request_handling_status_t handler(restinio::request_handle_t req) {
   auto headers = req->header();
-  auto protocol = parse_protocol(&headers);
+  auto protocol = extract_protocol(&headers);
+
+  if (protocol == AWSJsonProtocol1_0) {
+    return json_handler(&headers, req);
+  }
 
   req->create_response()
       .set_body("AAA(protocol: " + to_str(protocol) + ")")
       .done();
+
   return restinio::request_accepted();
 }
 
-AWSProtocol parse_protocol(restinio::http_request_header_t* headers) {
+restinio::request_handling_status_t json_handler(
+    restinio::http_request_header_t* headers, restinio::request_handle_t req) {
+  auto trace_id = extract_trace_id(headers).value_or("");
+  auto action = extract_action(headers);
+
+  if (!action.has_value()) {
+    return restinio::request_rejected();
+  }
+
+  req->create_response().set_body("JSON_HANDLER\n").done();
+
+  return restinio::request_accepted();
+}
+
+AWSProtocol extract_protocol(restinio::http_request_header_t* headers) {
   auto content_type = headers->opt_value_of(restinio::http_field::content_type);
   if (content_type.has_value() &&
       content_type.value() == AWS_JSON_PROTOCOL_1_0) {
     return AWSJsonProtocol1_0;
   }
-
   return AWSQueryProtocol;
+}
+
+std::optional<std::string> extract_trace_id(
+    restinio::http_request_header_t* headers) {
+  auto trace_id = headers->opt_value_of(AWS_TRACE_ID);
+  if (trace_id.has_value()) {
+    return trace_id.value().data();
+  }
+  return {};
+}
+
+std::optional<SQSAction> extract_action(
+    restinio::http_request_header_t* headers) {
+  auto target = headers->opt_value_of(AWS_TARGET);
+  if (target.has_value()) {
+    auto action_key = target.value().data();
+    try {
+      return AWS_SQS_ACTIONS.at(action_key);
+    } catch (const std::out_of_range&) {
+      return {};
+    }
+  }
+  return {};
 }
 
 std::string to_str(AWSProtocol protocol) {
@@ -27,6 +68,8 @@ std::string to_str(AWSProtocol protocol) {
       return "AWSQueryProtocol";
     case AWSJsonProtocol1_0:
       return "AWSJsonProtocol1.0";
+    default:
+      throw std::exception();
   }
 }
 }  // namespace sqscpp
--- src/router.hpp
+++ src/router.hpp
@@ -4,12 +4,23 @@
 #include <restinio/core.hpp>
 
 namespace sqscpp {
-const std::string AWS_JSON_PROTOCOL_1_0 = "application/x-amz-json-1.0";
-
 enum AWSProtocol { AWSQueryProtocol, AWSJsonProtocol1_0 };
+enum SQSAction { SQSListQueues };
+
+const std::string AWS_JSON_PROTOCOL_1_0 = "application/x-amz-json-1.0";
+const std::string AWS_TRACE_ID = "x-amzn-trace-id";
+const std::string AWS_TARGET = "x-amz-target";
+const std::map<std::string, SQSAction> AWS_SQS_ACTIONS = {
+    {"AmazonSQS.ListQueues", SQSListQueues}};
 
 restinio::request_handling_status_t handler(restinio::request_handle_t req);
-AWSProtocol parse_protocol(restinio::http_request_header_t* headers);
+restinio::request_handling_status_t json_handler(
+    restinio::http_request_header_t* headers, restinio::request_handle_t req);
+AWSProtocol extract_protocol(restinio::http_request_header_t* headers);
+std::optional<SQSAction> extract_action(
+    restinio::http_request_header_t* headers);
+std::optional<std::string> extract_trace_id(
+    restinio::http_request_header_t* headers);
 std::string to_str(AWSProtocol protocol);
 }  // namespace sqscpp
 
